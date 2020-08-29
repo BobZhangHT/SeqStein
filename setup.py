5b906df4da063caea0c67b42c1ce5567bb32073f
ZhangHT <30360988+BobZhangHT@users.noreply.github.com>
2020-08-29 16:31:38 +0800

    step file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='seqstein',
+      version='1.0',
+      description='A Sequential Stein\'s Method for Faster Training of Additive Index Models',
+      url='https://github.com/BobZhangHT/SeqStein',
+      author='Hengtao Zhang and Zebin Yang',
+      author_email='zhanght@connect.hku.hk, yangzb2010@hku.hk',
+      license='GPL',
+      packages=['seqstein'],
+      install_requires=['pandas',
+                        'rpy2',
+                        'matplotlib', 
+                        'numpy', 
+                        'sklearn', 
+                        'pygam'],
+      zip_safe=False)
