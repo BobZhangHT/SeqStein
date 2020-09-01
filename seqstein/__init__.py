8361cf450a9041cbe6beb7d33ac290bee5d324ef
bobzht <bobzht341@gmail.com>
2020-09-01 14:51:10 +0800

    fix a bug

--- seqstein/__init__.py
+++ seqstein/__init__.py
@@ -1,9 +1,8 @@
 from .mysim import SIM
 from .seqstein import SeqStein
-from .pprreg import PPRRegressor
 from .smspline import SMSplineRegressor
 
-__all__ = ["SIM", "SeqStein", "SMSplineRegressor", "PPRRegressor"]
+__all__ = ["SIM", "SeqStein", "SMSplineRegressor"]
 
 __version__ = '1.0.0'
 __author__ = 'Hengtao Zhang and Zebin Yang'
